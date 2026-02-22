bb3d09b712407c796dc02e02b8f59e94259dabae
Dibyadipan <dibya.icodes@gmail.com>
2026-02-22 10:22:59 +0530

    http-server - Built-in file server

--- http-server/main.go
+++ http-server/main.go
@@ -1,44 +1,14 @@
-//Manual File Handling version
+//using built-in file server
 package main
+
 import (
 	"log"
-	"mime"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func main(){
-	http.HandleFunc("/",fileHandler)
-	log.Println("Server running on port 3000")
+	fileServer := http.FileServer(http.Dir("./"))
+	http.Handle("/", fileServer)
+	log.Println("Server running on :3000")
 	log.Fatal(http.ListenAndServe(":3000",nil))
-}
-
-func fileHandler(w http.ResponseWriter, r *http.Request){
-	//Resolving file path
-	path:=r.URL.Path
-	if path == "/"{
-		path="/index.html"
-	}
-
-	//preventing directory traversal attacks (Security Check)
-	cleanPath := filepath.Clean("."+path)
-
-	//read file
-	data, err := os.ReadFile(cleanPath)
-	if err!=nil{
-		http.Error(w,"404 - File not found",http.StatusNotFound)
-		return
-	}
-
-	// detecting MIME type
-	ext := filepath.Ext(cleanPath)
-	contentType := mime.TypeByExtension(ext)
-	if contentType ==""{
-		contentType = "application/octet-stream"
-	}
-
-	w.Header().Set("Content-Type",contentType)
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
 }
